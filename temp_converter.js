8493f306cedbf48cbf9ccdeed0ad17658d3d6fe9
Azaniyah Shephard <session1@START-Coding-MBP-C02PP4G9G8WN.local>
2015-09-16 13:57:34 -0700

    no message

--- temp_converter.js
+++ temp_converter.js
@@ -2,11 +2,18 @@
 var fahrenheit = 32;
 
 // Enter conversion code below...
-var fahrenheitToCelsius = ...;
-var fahrenheitToKelvin = ...;
+var fahrenheitToCelsius = (fahrenheit - 32)/ 1.8;
+var fahrenheitToKelvin = (fahrenheit + 459.67)/ 1.8;
 
+var celsiusToFahrenheit = celsius * 1.8 + 32;
+var kelvinToFahrenheit = kelvin * 1.8 - 459.67;
 // Enter console.log statements below...
 
 // Repeat the above for Celsius and Kelvin...
 var celsius = 0;
 var kelvin = 273.15;
+
+console.log("Fahrenheit" + fahrenheitToCelsius);
+console.log("Celsius: " + celsiusToFahrenheit);
+console.log("Kelvin: " + kelvinToFahrenheit);
+console.log("Fahrenheit:" + fahrenheitToKelvin)
